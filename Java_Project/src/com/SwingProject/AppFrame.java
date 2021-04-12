064b248d6b1ece559ced7ccfbf1c77394a831d7e
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-12 19:41:11 -0400

    Add files via upload

---
+++ Java_Project/src/com/SwingProject/AppFrame.java
@@ -0,0 +1,12 @@
+package com.SwingProject;
+
+import javax.swing.*;
+
+public class AppFrame extends JFrame {
+    AppFrame(){
+        this.setSize(500, 500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+//        this.setLayout(null);
+    }
+}
