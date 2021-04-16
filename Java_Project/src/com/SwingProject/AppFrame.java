1032aebc4be717c5299163660cbf7f6936ed762f
Joseph Phelan <74159854+Yahzefi@users.noreply.github.com>
2021-04-15 20:39:16 -0400

    Update AppFrame.java

--- Java_Project/src/com/SwingProject/AppFrame.java
+++ Java_Project/src/com/SwingProject/AppFrame.java
@@ -3,10 +3,19 @@ package com.SwingProject;
 import javax.swing.*;
 
 public class AppFrame extends JFrame {
+
+    CalculatorPanel cPanel;
+
     AppFrame(){
+
+        cPanel = new CalculatorPanel();
+
+        this.add(cPanel);
+        this.setTitle("Java Swing Title");
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
-//        this.setLayout(null);
+        this.setLayout(null);
     }
 }
